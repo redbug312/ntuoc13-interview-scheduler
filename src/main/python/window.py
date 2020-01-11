c08c29cd481e4350433aef1b57c74e1c6ae42c7d
RedBug312 <redbug312@gmail.com>
2020-01-12 00:37:18 +0800

    Create layout prototype for application
    Open xlsx file when left button clicked

--- src/main/python/window.py
+++ src/main/python/window.py
@@ -1,24 +1,38 @@
 #!/usr/bin/env python3
+import openpyxl
 from PyQt5 import uic
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QMainWindow, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QTableWidgetItem
 
 
 class MainWindow(QMainWindow):
     def __init__(self, context):
         super().__init__()
         uic.loadUi(context.get_ui(), self)
-        self.pushButton.clicked.connect(self.open_file)
+        self.pushButton.clicked.connect(self.open_xlsx)
 
     @pyqtSlot()
-    def on_click(self):
-        print('Hello World')
-
-    @pyqtSlot()
-    def open_file(self):
-        dialog = QFileDialog()
-        dialog.setFileMode(QFileDialog.AnyFile)
-        dialog.selectNameFilter('Spreadsheets (*.xlsx)')
-        if dialog.exec_():
-            filenames = dialog.selectedFiles()
-            print(filenames)
+    def open_xlsx(self):
+        # file dialog for xlsx
+        # dialog = QFileDialog(parent=self)
+        # dialog.setFileMode(QFileDialog.ExistingFile)
+        # dialog.setNameFilter('Spreadsheets (*.xlsx)')
+        # if not dialog.exec_():
+        #     return
+        # xlsx = dialog.selectedFiles()[0]
+        xlsx = 'example.xlsx'
+        # load xlsx into data
+        wb = openpyxl.load_workbook(xlsx)
+        ws = wb.active
+        self.data = [[str(cell.value or '') for cell in rows]
+                     for rows in ws.iter_rows()]
+        # populate from data
+        rows_c, cols_c = len(self.data), len(self.data[0])
+        self.preview_table.setHorizontalHeaderLabels(self.data[0])
+        self.preview_table.setColumnCount(cols_c)
+        self.preview_table.setRowCount(rows_c)
+        self.filename_label.setText('載入 %d 列資料。' % rows_c)
+        for x, rows in enumerate(self.data):
+            for y, cell in enumerate(rows):
+                item = QTableWidgetItem(self.data[x][y])
+                self.preview_table.setItem(x, y, item)
